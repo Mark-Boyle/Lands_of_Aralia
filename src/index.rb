e3bbd44ccd581921ee20029d0fb108b8388205db
Mark Boyle <markboyle19@gmail.com>
2021-04-03 07:55:12 +1100

    Created loop to run the battle scene

--- src/index.rb
+++ src/index.rb
@@ -1,4 +1,5 @@
 require_relative('character')
+require_relative('enemy')
 
 #Introduction
 def display_intro_message
@@ -26,6 +27,7 @@ def select_name
     name = gets.chomp
     puts ' '
     puts "Welcome #{name}!"
+    name
 end
 
 def explain_available_items
@@ -52,10 +54,38 @@ enemy_attacks = ['breath fire', 'tail whip', 'slash']
 display_intro_message
 
 character = Character.new(select_name, 'human', 100, 0, 0, 0, actions)
+dragon = Enemy.new('Dragon', 150, 8)
+
+
 select_character
 character.display_character_info
 explain_available_items
 character.update_elf_stats
 
-
 #First Scene
+while dragon::enemy_health > 0 && character::character_health > 0
+    puts "Choose your action:"
+    input = gets.chomp
+    system 'clear'  
+    puts ' '
+    case input
+    when "Use Sword"
+        dragon::enemy_health = character_attack(dragon::enemy_health, character.generate_sword_damage)
+        puts ' '
+    when "Shoot Arrow"
+        dragon::enemy_health = character_attack(dragon::enemy_health, character.generate_archery_damage)
+        puts ' '
+    when "Search Area"
+        character.search_area
+    end
+
+    character::character_health = enemy_attack(character::character_health, dragon::generate_attack_damage - character::armour_rating, enemy_attacks)
+    puts ' '
+    display_info(dragon::enemy_health, character::character_health)
+end
+
+if character::character_health > 0
+    puts 'You won!'
+else 
+    puts 'You lost!'
+end
